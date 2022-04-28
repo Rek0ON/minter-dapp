55347a451b9baf5fed796293ae0ab9a8d3d0deef
Rek0ON <baranovsky_pavel@bk.ru>
2022-04-28 17:13:24 +0300

    fix

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x470C21F057ee51e65E143B038965C7EDcF118848"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
